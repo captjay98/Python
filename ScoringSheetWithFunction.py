fc40ff74649088ee5410473bef2c197dc5ad40f6
captjay98 <jamalibrahim0@gmail.com>
2021-08-26 10:01:37 +0100

    Tweaks

--- ScoringSheetWithFunction.py
+++ ScoringSheetWithFunction.py
@@ -1,13 +1,18 @@
+#Keeping the scoring in a loop
 while True:
 
+ #To catch and handle abberations
  try:
 
+   #The score input
    s = float(input('enter score between 0-100\n'))
 
+   #A function to store the scores
    def computegrade():
     grade = s
     return s
 
+   #The scores and grading
    if s > 100 or s < 0:
       print ('enter a valid score')
    elif s>=90:
@@ -22,6 +27,6 @@ while True:
       print('F')
    else:
       print('enter a valid score please')
-
+ #Catching exceptions
  except:
    print('Please input Score!')
