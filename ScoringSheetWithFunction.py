bd50a6f7939ccebad1346ba15a4c037904724323
captjay98 <jamalibrahim0@gmail.com>
2021-08-26 09:51:05 +0100

    Tweaks

--- ScoringSheetWithFunction.py
+++ ScoringSheetWithFunction.py
@@ -1,22 +1,22 @@
 while True:
  try:
-   s = input('enter score between 0.0-1.0\n')
+   s = input('enter score between 0-100\n')
    s = float(s)
 
    def computegrade():
     grade = s
     return s
-   if s > 1.0 or s < 0.0:
+   if s > 100 or s < 0:
       print ('enter a valid score')
-   elif s>=0.9:
+   elif s>=90:
       print('A')
-   elif s>=0.8:
+   elif s>=80:
       print('B')
-   elif s>=0.7:
+   elif s>=70:
       print('C')
-   elif s>0.6:
+   elif s>50:
       print('D')
-   elif s<0.6:
+   elif s<50:
       print('F')
    else:
       print('enter a valid score please')
