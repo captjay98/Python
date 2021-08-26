d4b1a36b205af49c28c4072bbe6f8a69f11982d8
captjay98 <54381061+captjay98@users.noreply.github.com>
2021-08-26 09:12:17 +0100

    Add files via upload

---
+++ JAMALIBRAHIMUMAR.py
@@ -0,0 +1,17 @@
+my_name = 'JAMAL IBRAHIM UMAR'
+my_age = 22
+my_height = 182  # CM
+my_weight = 66 # KG
+my_eyes = 'BROWN'
+my_teeth = 'WHITE'
+my_hair = 'BLACK'
+
+print(f"let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} KILOGRAMS heavy")
+print("Actually that's not too heavy")
+print(f"He's got {my_eyes} eyes and {my_hair} hair")
+print(f"His teeth are usually {my_teeth} ")
+
+total = my_age + my_height
+print(f"if i add {my_age}, {my_height}, and {my_weight} i get {total}")
