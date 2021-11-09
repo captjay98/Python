d9cedb82f843faf3183cd1ad32465dbd40420511
captjay98 <jamalibrahim0@gmail.com>
2021-11-09 04:37:56 +0100

    Corrected the maths

--- Pay.py
+++ Pay.py
@@ -1,16 +1,19 @@
-try:
-    hours = input('Enter Hours\n')
-    hours = int(hours)
+while True:
+    try:
+        hours = input('Enter Hours\n')
+        hours = int(hours)
 
-    rate = input('Enter Rate\n')
-    rate = float(rate)
+        rate = input('Enter Rate\n')
+        rate = float(rate)
 
-    if hours > 40:
-        pay = (hours * (rate + 0.5556))
-        print(pay)
+        if hours > 40:
+            pay = hours * rate
+            overtime = (hours - 40.0) * (rate * 0.5)
+            overtime_pay = pay + overtime
+            print(overtime_pay)
 
-    else:
-        pay = (hours * rate)
-        print(pay)
-except:
-    print('Error, Please enter numeric input')
+        else:
+            pay = hours * rate
+            print(pay)
+    except:
+        print('Error, Please enter numeric input')
