ee321fe28bbaceb45072adcfc26a637399b07a40
captjay98 <54381061+captjay98@users.noreply.github.com>
2022-02-01 00:23:45 +0100

    tiiii

---
+++ A.py
@@ -0,0 +1,13 @@
+(Name, Age) = ('Jamal', 23)
+
+print(Name, Age)
+
+dic = {}
+dic['name'] = 'Jamal'
+dic['age'] = 98
+
+for (key, value) in dic.items():
+    print(key, value)
+
+tups = dic.items()
+print(tups)
