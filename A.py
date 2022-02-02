d284e18333b8bea0164e491251c9d3d4b1144c31
captjay98 <54381061+captjay98@users.noreply.github.com>
2022-02-02 01:54:44 +0100

    Hit a snag

--- A.py
+++ A.py
@@ -1,57 +1,17 @@
-import re
+# Sockets
 
-file = open('C:\ppy\mbox.txt')
+import socket
 
-for line in file:
-    line = line.rstrip()
-    if re.search('^From:', line):
-        print(line)
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data,pr4e.org', 80))
 
-x = 'My 5 Favourite numbers are 98 and 0'
-y = re.findall('[0-9]+', x)
-print(y)
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
+mysock.send(cmd)
 
-# Greedy
-x = 'From: Using the : character'
-y = re.findall('^F.+:', x)
-print(y)
-
-# Not Greedy
-x = 'From: Using the : character'
-y = re.findall('^F.+?:', x)
-print(y)
-
-# Extract Email
-x = 'From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16 2008'
-y = re.findall('\S+@\S+', x)
-print(y)
-
-# Extract Email
-x = 'From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16 2008'
-y = re.findall('^From (\S+@\S+)', x)
-print(y)
-
-# REMOVE HOST
-x = 'From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16 2008'
-y = re.findall('@([^ ]*)', x)
-print(y)
-
-x = 'From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16 2008'
-y = re.findall('^From .*@([^ ]*)', x)
-print(y)
-
-# EXTRACTTION
-file = open('C:\ppy\mbox.txt')
-numlist = []
-for line in file:
-    line = line.rstrip()
-    stuff = re.findall('^X-DSPAM-Confidence: ([0-9.]+)', line)
-    if len(stuff) != 1:
-        continue
-    num = float(stuff[0])
-    numlist.append(num)
-print('Maximum:', max(numlist))
-
-x = 'We just received $10.00 for cookies.'
-y = re.findall('\$[0-9.]+', x)
-print(y)
+while True:
+    data = mysock.recv(512)
+    if (len(data) < 1):
+        break
+    print(data.decode())
+mysock.close()
+# HTTP
