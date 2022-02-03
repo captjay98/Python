997fa68159fa591c6bfe61eb69d37715bf5bdf97
captjay98 <54381061+captjay98@users.noreply.github.com>
2022-02-03 05:44:02 +0100

    XML is fun

--- A.py
+++ A.py
@@ -1,20 +1,28 @@
 
-# urllib
+# xml
 
-import urllib.request
-import urllib.parse
-import urllib.error
-from bs4 import BeautifulSoup
-import ssl
+import xml.etree.ElementTree as ET
 
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
+input = '''
 
-url = input('Enter url==')
-html = urllib.request.urlopen(url).read()
-soup = BeautifulSoup(html, 'html.parser')
+<stuff>
+  <users>
+    <user x="5">
+       <name>Jay</name>
+       <id>008</id>
+    </user>
+    <user x="6">
+       <name>CAP</name>
+       <id>009</id>
+    </user>
+ </users>
+</stuff>'''
 
-tags = soup('a')
-for tag in tags:
-    print(tag.get('href', None))
+stuff = ET.fromstring(input)
+lst = stuff.findall('users/user')
+print('User Count', len(lst))
+
+for item in lst:
+    print('Name:', stuff.find('name').text)
+    print('Attribute:', item.get("X"))
+    print('Id', item.find('id').text)
