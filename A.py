91cd5df45c2358e402feba77ce2c50732e48a47a
captjay98 <54381061+captjay98@users.noreply.github.com>
2022-02-03 05:49:52 +0100

    Still on xml

--- A.py
+++ A.py
@@ -8,14 +8,14 @@ input = '''
 <stuff>
   <users>
     <user x="5">
-       <name>Jay</name>
        <id>008</id>
+       <name>Jay</name>
     </user>
     <user x="6">
-       <name>CAP</name>
        <id>009</id>
+       <name>CAP</name>
     </user>
- </users>
+  </users>
 </stuff>'''
 
 stuff = ET.fromstring(input)
@@ -23,6 +23,6 @@ lst = stuff.findall('users/user')
 print('User Count', len(lst))
 
 for item in lst:
-    print('Name:', stuff.find('name').text)
-    print('Attribute:', item.get("X"))
+    print('Name:', item.find('name').text)
     print('Id', item.find('id').text)
+    print('Attribute:', item.get("X"))
