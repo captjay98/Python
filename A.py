0ce6ea8b20b12c9d11d916e2823aba611f779a23
captjay98 <54381061+captjay98@users.noreply.github.com>
2022-02-03 04:17:24 +0100

    webscraping is cool

--- A.py
+++ A.py
@@ -5,6 +5,11 @@ import urllib.request
 import urllib.parse
 import urllib.error
 from bs4 import BeautifulSoup
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter url==')
 html = urllib.request.urlopen(url).read()
