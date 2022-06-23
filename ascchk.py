eb283e582664b8c97cb171e036d6322f55759dfb
captjay98 <captjay98@gmail.com>
2022-06-23 21:05:57 +0100

    Prints the Asciii equivalent of the nummber

--- ascchk.py
+++ ascchk.py
@@ -4,7 +4,12 @@ def asc(char):
 
     return ("{:s}".format(chr(char)))
 
+while True:
+    x = input("Enter Number:-")
+    x = int(x)
 
-x = input("Enter Number")
-x = int(x)
-print(f" Ascii number {x} is {asc(x)}")
+    if x < 0 or x > 150:
+         print(f"{x} is out of range")
+
+    else:
+        print(f" Ascii number {x} is {asc(x)}")
