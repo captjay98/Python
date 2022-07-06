79444924191e112f283dba79b840edc6bba89962
captjay98 <captjay98@gmail.com>
2022-07-06 23:37:49 +0100

    A square class

--- OOP/square.py
+++ OOP/square.py
@@ -1,53 +1,50 @@
 #!/usr/bin/python3
 
-"""Define a Square."""
+"""Define a Shape."""
 
+from re import A
+import turtle
 
-class Square:
+class Shapes:
     """Represent a Square."""
     
     def __init__(self, height="0", width="0"):
         self.height = height
         self.width = width
+        self.sides = 4
         
     @property
     def height(self):
         print("Retrieving height")
-        
         return self.__height
     
     @height.setter
-    def height(self, value):
-        
-        
+    def height(self, value):        
         self.__height = value
 
-            
     @property
     def width(self): 
         print("Retrieving Width")
-        
         return self.__width
     
     @width.setter
     def width(self, value):
-        
-
         self.__width = value
 
     def getArea(self):
         return int(self.__width) * int(self.__height)
-    
-    
-aSquare = Square()
 
-height = input("Enter height :-")
-width = input("Enter width:-")
+    def draw(self):
+        """Function That drawa a shape using turtle"""
+        for i in range(self.sides):
+            turtle.forward(self.__width)
+            turtle.right(self.__height)
+        turtle.done()
 
-aSquare.height = height
-aSquare.width = width
+aSquare = Shapes(50,180)
+#aRect = Shapes()
+print(aSquare)
+print(aSquare.height)
+print(aSquare.width)
 
-print("Height: ", aSquare.height)
-print("Width: ", aSquare.width)
-
-print("The Area is", aSquare.getArea())
+aSquare.draw()
