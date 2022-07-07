ea8f74d61e1209c671b5cd540544b27910cea691
captjay98 <captjay98@gmail.com>
2022-07-07 04:34:07 +0100

    Added a Polygon and cleaned up

--- OOP/square.py
+++ OOP/square.py
@@ -2,16 +2,18 @@
 
 """Define a Shape."""
 
-from re import A
+
 import turtle
+
+
 class Shapes:
     """Represent a Square."""
 
-    def __init__(self,sides=4, height=0, width=0):
+    def __init__(self, sides=4, height=0, width=0):
         self.sides = sides
         self.__height = height
         self.__width = width
-        self.interior = (self.sides -2) * 180
+        self.interior = (self.sides - 2) * 180
         self.angle = self.interior/self.sides
 
     @property
@@ -34,35 +36,35 @@ class Shapes:
 
     def draw(self):
         """Function That drawa a shape using turtle"""
-        for i in range(4):
+        for i in range(self.sides):
             turtle.forward(self.__width)
             turtle.right(180-self.angle)
         turtle.done()
-    
-        
 
     def getArea(self):
         return int(self.__width) * int(self.__height)
 
     def __str__(self):
-        return "Height of {} = {}, Width = {} and sides = {}".format(self.name, self.__height, self.__width, self.sides)
+        return ("Height of {} = {}, Width = {} and sides = {}"
+                .format(self.name, self.__height, self.__width, self.sides))
+
 
 class Square(Shapes):
-    def __init__(self,sides=4, height=0, width=0):
+    def __init__(self, sides=4, height=0, width=0):
         super().__init__(sides, height, width)
         self.sides = sides
         self.__height = height
         self.__width = width
         self.name = "Square"
 
+
 class Rectangle(Shapes):
-    def __init__(self,sides=4, height=50, width=0):
+    def __init__(self, sides=4, height=0, width=0):
         super().__init__(sides, height, width)
         self.sides = sides
         self.__height = height
         self.__width = width
         self.name = "Reactangle"
-
 
     def draw(self):
         """Function That drawa a shape using turtle"""
@@ -77,15 +79,17 @@ class Rectangle(Shapes):
         turtle.done()
 
 
+class Polygon(Shapes):
+    def __init__(self, sides=5, height=0, width=0):
+        super().__init__(sides, height, width)
+        self.sides = sides
+        self.__height = height
+        self.__width = width
+        self.name = "Polygon"
+
+
 aSquare = Square(4, 300, 300)
-print(aSquare)
-
 print("\n")
-
 aRect = Rectangle(4, 200, 400)
-
-print(aRect)
-
-
-
-
+print("\n")
+poly = Polygon(5, 200, 200)
