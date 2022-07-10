5da4c5d700b71d43488aa86eeebbcca5f41ce2c6
captjay98 <captjay98@gmail.com>
2022-07-10 14:02:03 +0100

    Prints the ascii equiv of a string then the real string

---
+++ practice/strord.c
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+import code
+
+
+x = input("Enter a string to hide in upper case :- ")
+sec = ""
+
+for i in x:
+    sec += str(ord(i) - 23)
+print("Sec = ", sec)
+
+norm = ""
+
+for c in range(0, len(sec) -1, 2):
+    code = sec[c] + sec[c + 1]
+    norm += chr(int(code) + 23)
+print("Original Message = ", norm)
