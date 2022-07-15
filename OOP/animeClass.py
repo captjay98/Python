b729e8ab71047dedaf5ab019552ba53d54089f7d
captjay98 <captjay98@gmail.com>
2022-07-15 22:12:04 +0100

    Split the class and subclasses

--- OOP/animeClass.py
+++ OOP/animeClass.py
@@ -1,86 +1,10 @@
 #!/usr/bin/python3
 
+
 from re import L
 from tracemalloc import start
 from unicodedata import name
-
-
-class Ninja():
-
-    _alias = "They are all Formidable Hidden Villages"
-    __members = 0
-    battles = 0
-
-    def __init__(self, name="", rank="", powerLevel=0):
-        self.name = name
-        self.rank = rank
-        self.__powerLevel = powerLevel
-        
-        Ninja.__members += 1
-
-    @staticmethod
-    def participants():
-        return Ninja.__members
-
-    @classmethod
-    def about(cls):
-        return(f"{cls._alias}")
-
-    @classmethod
-    def numofBattles(cls, val):
-        cls.battles = val
-
-    def knownFor(self):
-        if self.name == "Naruto":
-            print(f"{self.name} is known for being the "
-                "Number one Crackhead Ninja")
-        elif self.name == "Rock Lee":
-            print(f"{self.name} is known for being the "
-                "The Most Enthusiastic Ninja")
-        else:
-            print(f"{self.name} is known for what they're known for")
-
-    @property
-    def powerLevel(self):
-        return self.__powerLevel
-
-    @powerLevel.setter
-    def powerLevel(self, value):
-        self.__powerLevel = value
-
-    def strength(self, speciality):
-        if self.name == "Naruto":
-            print(f"{self.name} is known for being the "
-                "Number one Crackhead Ninja ")
-            print(f"He loves using the {speciality}")
-
-        elif self.name == "Rock Lee":
-            print(f"{self.name} is known for being the "
-                "The Most Enthusiastic Ninja")
-            print(f"He loves using the {speciality}")
-        else:
-            print(f"{self.name} is known for what they're known for")
-
-
-    def details(self):
-        detail = ("His name is {} He is a {} and has a power level of {}"
-                .format(self.name, self.rank, self.__powerLevel))
-        return  detail
-
-    def __str__(self):
-        detail = ("His name is {} He is a {} and has a power level of {}"
-                .format(self.name, self.rank, self.__powerLevel))
-        return detail
-
-    def __eq__(self, object):
-        return self.name == object.name and self.rank == object.rank
-
-    @staticmethod
-    def start_powwer(powerLevel):
-        if powerLevel < 400:
-            return "You,re Out"
-        else:
-            return ("You're INN")
+from ninjaClass import Ninja
 
 
 class LeafVillage(Ninja):
@@ -131,6 +55,14 @@ temari = SandVillage("Temari", "Chunin", 500, "Wind Style")
 Ninja.numofBattles(50)
 print(Ninja.battles)
 
+print(Ninja.participants())
+
+naruto.die()
+temari.die()
+lee.die()
+kiba.die()
+gaara.die()
+
 """
 #print(narut)
 print(naruto.inLeaf())
@@ -157,7 +89,7 @@ naruto.powerLevel = 900
 #lee.strength("Leaf Hurricane")
 print(naruto)
 print(gaara)
- 
+
 gaara.powerLevel = 800
 print(gaara)
 
@@ -167,6 +99,8 @@ print(LeafVillage.about())
 print(SandVillage.about())
 
 """
+
+print(Ninja.participants())
 
 
 
