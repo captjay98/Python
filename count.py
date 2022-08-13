69d23f94083c8df00994879d580fce5cf8fd1beb
captjay98WSL <captjay98@gmail.com>
2022-08-13 23:46:45 +0100

    alx thingy

--- count.py
+++ count.py
@@ -1,4 +1,6 @@
-words = ('Of all the hard things we have to do everyday, the most difficult one of them all is to keep going” this line resonated with me more than any other in the video, i know how hard it can get, sometimes you get stuck to the point that it feels like there’s never going to be a way out, giving up is the only option, but more times than not, that happens on the verge of an explosive moment a little perseverance and patience makes the hard moment worth it. GIVING UP IS NEVER AN OPTION, AS LONG AS THERE’S LIFE, THERE’S HOPE.')
+#!/usr/bin/python3
+words = ('Of all the hard things we have to do everyday, the most difficult one of them all is to keep going” this line resonated with me more than any other in the video, i know how hard it can get, sometimes you get stuck to the point that it feels like there’s never going to be a way out, giving up is the only option, but more times than not, that happens on the verge of an explosive moment a little perseverance and patience makes the hard moment worth it. GIVING UP IS NEVER AN OPTION, AS LONG AS THERE’S LIFE, THERE’S HOPE')
+
 
 words = words.split(" ")
 
