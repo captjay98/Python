94fc2889e362d008cce7f019b421cfcf546040ea
captjay98WSL <captjay98@gmail.com>
2022-10-03 14:16:27 +0100

    dunno

--- SQLalchemy/query.py
+++ SQLalchemy/query.py
@@ -18,7 +18,17 @@ session.query(Song).get(9)
 
 print(session.query(Song).filter(Song.name =='Too Sad to Cry').all())
 print(session.query(Song).filter(Song.name == 'Too Sad to Cry'))
-'''
 data = session.query(Artist.stage_name).join(Song.name).all()
+
 for item in data:
     print(Artist.stage_name, Album.name)
+'''
+
+data = session.query(Artist, Album, Song).filter(Artist.id == Album.artist_id).all()
+for item, it, ik in data:
+    print("{:^5s} - {:^5s}".format(item.stage_name, it.name))    
+
+d = session.query(Album, Song).filter(Album.id == Song.album_id).all()
+
+for i, j in d:
+    print(i.name, j.name)
